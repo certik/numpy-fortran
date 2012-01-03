45a2215780e758cbb8c622caf7a3fda3d2c1f35f
Ondrej Certik <ondrej@certik.cz>
2012-01-02 17:08:00 -0800

    Use data

--- fortran/a.f90
+++ fortran/a.f90
@@ -3,21 +3,31 @@ use types, only: dp
 use compute, only: init, register_func, run, eq
 
 type(eq) :: d
+character(len=1), allocatable :: data(:)
+integer :: l
 
 call init(d)
 call register_func(d, derivs)
-call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10)
+l = size(transfer(1, data))
+allocate(data(l))
+data = transfer(1, data)
+call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10, data)
 print *
-call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10)
+deallocate(data)
+l = size(transfer(2, data))
+allocate(data(l))
+data = transfer(2, data)
+call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10, data)
 
 contains
 
-function derivs(x) result(y)
+function derivs(x, data) result(y)
 use types, only: dp
 real(dp), intent(in) :: x(2)
+character(len=1), intent(in) :: data(:)
 real(dp) :: y(2)
 integer :: eq_type
-eq_type = 1
+eq_type = transfer(data, eq_type)
 if (eq_type == 1) then
     y(1) = -x(2)
     y(2) = x(1)
--- fortran/compute.f90
+++ fortran/compute.f90
@@ -5,9 +5,10 @@ private
 public init, register_func, run, eq
 
 abstract interface
-    function derivs(x) result(y)
+    function derivs(x, data) result(y)
     use types, only: dp
     real(dp), intent(in) :: x(2)
+    character(len=1), intent(in) :: data(:)
     real(dp) :: y(2)
     end function
 end interface
@@ -30,10 +31,11 @@ procedure(derivs) :: func
 d%func => func
 end subroutine
 
-subroutine run(d, x0, dt, n_steps)
+subroutine run(d, x0, dt, n_steps, data)
 type(eq), intent(in) :: d
 real(dp), intent(in) :: x0(2), dt
 integer, intent(in) :: n_steps
+character(len=1), intent(in) :: data(:)
 
 real(dp) :: x(2), dx(2), t
 integer :: i
@@ -43,7 +45,7 @@ end if
 x = x0
 t = 0
 do i = 1, n_steps
-    dx = d%func(x)
+    dx = d%func(x, data)
     print "(f10.6, f10.6)", x
     x = x + dx * dt
     t = t + dt

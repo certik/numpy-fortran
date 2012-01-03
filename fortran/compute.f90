38da35dbf80621cd6eaf30166f6996d4abd55d7b
Ondrej Certik <ondrej@certik.cz>
2012-01-02 16:46:26 -0800

    Fix

--- fortran/compute.f90
+++ fortran/compute.f90
@@ -1,11 +1,12 @@
 module compute
-
+use types, only: dp
 implicit none
 private
-public init, register_func, run
+public init, register_func, run, dp
 
 abstract interface
     function derivs(x) result(y)
+    use types, only: dp
     real(dp), intent(in) :: x(2)
     real(dp) :: y(2)
     end function
@@ -13,34 +14,40 @@ end interface
 
 type eq
     integer :: i
-    procedure(derivs) :: func
+    procedure(derivs), nopass, pointer :: func
 end type
 
 contains
 
 subroutine init(d)
 type(eq), intent(inout) :: d
-d%func = NULL;
+d%func => NULL()
 end subroutine
 
 subroutine register_func(d, func)
 type(eq), intent(inout) :: d
-procedure(derivs), intent(in) :: func
-d%func => func;
+procedure(derivs), pointer, intent(in) :: func
+d%func => func
 end subroutine
 
-subroutine (d, x0, dt, n_steps)
+subroutine run(d, x0, dt, n_steps)
 type(eq), intent(in) :: d
 real(dp), intent(in) :: x0(2), dt
 integer, intent(in) :: n_steps
 
 real(dp) :: x(2), dx(2), t
+integer :: i
+if (.not. associated(d%func)) then
+    print *, "d%func is not associated"
+end if
 x = x0
 t = 0
 do i = 1, n_steps
     dx = d%func(x)
     print *, x
-    x += dx * dt
-    t += dt
+    x = x + dx * dt
+    t = t + dt
 end do
 end subroutine
+
+end module

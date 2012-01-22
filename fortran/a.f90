c9e959085308d5699068a8b548a7375bbea79b03
Ondrej Certik <ondrej@certik.cz>
2012-01-22 14:38:26 -0800

    Update the Fortran program according to the C one

--- fortran/a.f90
+++ fortran/a.f90
@@ -1,54 +1,99 @@
-program a
+module my_data_type
+! This module exports the data type and conversion routines
+! to convert a pointer to untyped data(:)
+! It is only used from the main program a.f90
 use types, only: dp
-use compute, only: init, register_func, run, eq
+implicit none
+private
+! Only these symbols are available from the main program:
+public my_data, type2data, data2type
 
-type(eq) :: d
-integer, allocatable :: data(:)
+type my_data
+    ! Material coefficients:
+    real(dp) :: a11, a12, a21, a22
+    ! There can be a lot of variables and big arrays here, this needs
+    ! to be passed around by reference.
+end type
 
-call init(d)
-call register_func(d, derivs)
-call type2data(1, data)
-call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10, data)
-print *
-call type2data(2, data)
-call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10, data)
+type my_data_ptr
+    type(my_data), pointer :: ptr
+end type
 
 contains
 
-! Here the "type" is just an integer, but we can use any type
-subroutine type2data(i, data)
-integer, intent(in) :: i
-integer, allocatable, intent(out) :: data(:)
-integer :: l
-l = size(transfer(i, data))
-allocate(data(l))
-data = transfer(i, data)
-end subroutine
-
-subroutine data2type(data, i)
+function data2type(data) result(d)
 integer, intent(in) :: data(:)
-integer, intent(out) :: i
-i = transfer(data, i)
-end subroutine
+type(my_data), pointer :: d
+type(my_data_ptr) :: tmp
+tmp = transfer(data, tmp)
+d => tmp%ptr
+end function
 
+pure integer function pointer_length() result(l)
+type(my_data_ptr) :: p
+integer, allocatable :: data(:)
+l = size(transfer(p, data))
+end function
+
+function type2data(d) result(data)
+type(my_data), intent(in), target :: d
+integer :: data(pointer_length())
+type(my_data_ptr) :: tmp
+tmp%ptr => d
+data = transfer(tmp, data)
+end function
+
+end module
+
+
+
+program a
+use types, only: dp
+use compute, only: init, register_func, run, eq, destroy, get_context
+use my_data_type, only: my_data, type2data, data2type
+
+type(eq), pointer :: d
+type(my_data), target :: data1, data2
+
+data1%a11 = 0
+data1%a12 = -1
+data1%a21 = 1
+data1%a22 = 0
+
+data2%a11 = 0
+data2%a12 = 1
+data2%a21 = 1
+data2%a22 = 0
+
+call init(d)
+call register_func(d, derivs, type2data(data1))
+call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10)
+call print_material_parameters(d)
+print *
+call register_func(d, derivs, type2data(data2))
+call run(d, [0.0_dp, 1.0_dp], 0.1_dp, 10)
+call print_material_parameters(d)
+call destroy(d)
+
+contains
+
+subroutine print_material_parameters(d)
+type(eq), intent(in) :: d
+type(my_data), pointer :: ctx
+ctx => data2type(get_context(d))
+print "('Material parameters: ', f0.6, ' ', f0.6, ' ', f0.6, ' ', f0.6)", &
+    ctx%a11, ctx%a12, ctx%a21, ctx%a22
+end subroutine
 
 function derivs(x, data) result(y)
 use types, only: dp
 real(dp), intent(in) :: x(2)
 integer, intent(in) :: data(:)
 real(dp) :: y(2)
-integer :: eq_type
-call data2type(data, eq_type)
-if (eq_type == 1) then
-    y(1) = -x(2)
-    y(2) = x(1)
-else if (eq_type == 2) then
-    y(1) = x(2)
-    y(2) = x(1)
-else
-    print *, "Not Implemented"
-    stop 1
-end if
+type(my_data), pointer :: d
+d => data2type(data)
+y(1) = d%a11 * x(1) + d%a12 * x(2)
+y(2) = d%a21 * x(1) + d%a22 * x(2)
 end function
 
 end program
--- fortran/compute.f90
+++ fortran/compute.f90
@@ -2,7 +2,7 @@ module compute
 use types, only: dp
 implicit none
 private
-public init, register_func, run, eq
+public init, destroy, register_func, run, eq, get_context
 
 abstract interface
     function derivs(x, data) result(y)
@@ -15,28 +15,43 @@ abstract interface
 end interface
 
 type eq
-    integer :: i
+    integer, allocatable :: data(:)
     procedure(derivs), nopass, pointer :: func
 end type
 
 contains
 
 subroutine init(d)
-type(eq), intent(inout) :: d
+type(eq), pointer, intent(inout) :: d
+allocate(d)
 d%func => NULL()
 end subroutine
 
-subroutine register_func(d, func)
-type(eq), intent(inout) :: d
-procedure(derivs) :: func
-d%func => func
+subroutine destroy(d)
+type(eq), pointer, intent(inout) :: d
+deallocate(d)
 end subroutine
 
-subroutine run(d, x0, dt, n_steps, data)
+subroutine register_func(d, func, data)
+type(eq), intent(inout) :: d
+procedure(derivs) :: func
+integer, intent(in) :: data(:)
+d%func => func
+if (allocated(d%data)) deallocate(d%data)
+allocate(d%data(size(data)))
+d%data = data
+end subroutine
+
+function get_context(d) result(data)
+type(eq), intent(in) :: d
+integer :: data(size(d%data))
+data = d%data
+end function
+
+subroutine run(d, x0, dt, n_steps)
 type(eq), intent(in) :: d
 real(dp), intent(in) :: x0(2), dt
 integer, intent(in) :: n_steps
-integer, intent(in) :: data(:)
 
 real(dp) :: x(2), dx(2), t
 integer :: i
@@ -46,7 +61,7 @@ end if
 x = x0
 t = 0
 do i = 1, n_steps
-    dx = d%func(x, data)
+    dx = d%func(x, d%data)
     print "(f10.6, f10.6)", x
     x = x + dx * dt
     t = t + dt
